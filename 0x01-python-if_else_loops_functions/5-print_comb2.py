112b6a3c10c78a4978cf09f0ba60b959180208c7
marwan <marwanhamdaoui2020@gmail.com>
2024-01-21 13:29:36 -0800

    lets go

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print("{0:2d}\n".format(i))
+        print("{0:02d}\n".format(i), end="")
         break
     else:
-        print("{0:2d}, ".format(i))
+        print("{0:02d}, ".format(i), end="")
