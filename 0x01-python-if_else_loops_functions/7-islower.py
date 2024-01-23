7115c20bead0903a7c75fbc5a6e1ae55bd8173ae
marwan <marwanhamdaoui2020@gmail.com>
2024-01-23 06:09:03 -0800

    lets go

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
+
 def islower(c):
-#    if (ord(c)>= 97 and ord(c) <= 123):
- #       return True
-  #  elif (ord(c)>= 65 and ord(c) <= 91):
-   #     return False
-   if ord(c) == range(97, 123):
-       return True
-   elif ord(c) == range(65, 91):
-       return False
+    if ord(c) in range(97, 123):
+        return True
+    elif ord(c) in range(65, 91):
+        return False
