0fe5e19e86b7afbc187338a2ddd9a74201649609
marwan <marwanhamdaoui2020@gmail.com>
2024-02-04 14:11:20 -0800

    lets go immmm tired

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import *
+    from sys import argv
     if len(argv) == 1:
         print("{} arguments.".format(len(argv) - 1))
     else:
