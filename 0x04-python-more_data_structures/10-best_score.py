a09590a4a5e316da63423ae185f4d02fa022070a
marwan <marwanhamdaoui2020@gmail.com>
2024-03-18 17:28:14 -0700

    lets go

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        sorted_keys = sorted(a_dictionary)
+        list = []
+        for i, v in a_dictionary.items():
+            list.append(v)
+        s_list = sorted(list)
+        max_value = s_list[-1]
+        return max_value
