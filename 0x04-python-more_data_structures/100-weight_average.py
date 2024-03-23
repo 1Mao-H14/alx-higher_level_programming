7752cd8ad28fa08d040020fc46bc2cc1315c14d5
marwan <marwanhamdaoui2020@gmail.com>
2024-03-23 16:48:52 -0700

    lets go

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+head = 0
+down = 0
 def weight_average(my_list=[]):
-    head = 0
-    down = 0
-    for i ,v in (my_list):
+    global head, down
+    for i, v in (my_list):
         head += (i*v)
         down += v
         resulte = head/down
