0b0a5b3b5fb47684f862bd21ae1652f599c60145
marwan <marwanhamdaoui2020@gmail.com>
2024-04-11 07:17:05 -0700

    lets go

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 import sys
+
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except ZeroDivisionError as ze:
-        print("Exception: {}".format(ze), file= sys.stderr )
+        print("Exception: {}".format(ze), file=sys.stderr)
         return None
     except IndexError as ie:
-        print("Exception: {}".format(ie), file= sys.stderr)
+        print("Exception: {}".format(ie), file=sys.stderr)
         return None
