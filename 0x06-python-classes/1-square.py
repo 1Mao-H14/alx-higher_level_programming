40cdd2c7047ea01f996e4e358e9142a6673466bc
1Mao-H14 <marwanhamdaoui2020@gmail.com>
2024-05-23 13:43:44 -0700

    first touch

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """Initializes the Square with a size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
