ad31adba57832926c9088113a865022c65fda965
marwan <marwanhamdaoui2020@gmail.com>
2024-07-24 12:06:58 -0700

    lets go

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+A module thats thats print strings
+    Functions:
+        say_my_name(first_name, last_name=""): a function thats print name last name
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    A function thats prints My name is <first name> <last name>
+    Args:
+        first_name(str): the first string
+        last_name(str): the second string
+    Returns:
+        return nothing
+    Raises:
+        TypeError: in case of the first_name or the first string is not string 
+
+    """
     if not(isinstance(first_name, str)):
         raise TypeError('first_name must be a string')
     elif not(isinstance(last_name, str)):
