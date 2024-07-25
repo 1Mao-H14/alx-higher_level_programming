76683c7986e16068ebe5b56714b665f5e7597fa5
marwan <marwanhamdaoui2020@gmail.com>
2024-07-24 17:28:06 -0700

    lets go

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+A module for that printing a square with the character  # .
+    Functions:
+        print_square(size): print the a square with the character  #
+"""
+
+
+def print_square(size):
+    """
+    a function thats prints a square with the character #.
+    Args:
+    size(int): the size of rectangle
+    Return:
+        str : a square of # caracters
+    Raises:
+         TypeError : if size not an integer or its float
+         ValueError: if size not >= 0
+    """
+
+    if (isinstance(size, float) and (size < 0)):
+        raise TypeError('size must be an integer')
+    elif not(isinstance(size, int)):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    else:
+        for h in range(size):
+            for i in range(size):
+                print('#', end='')
+            print()
