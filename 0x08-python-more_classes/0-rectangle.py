6c7f792621a48010eee1b1ef18e24c10a581f38e
marwan <marwanhamdaoui2020@gmail.com>
2024-09-04 14:24:18 -0700

    task 1 : besically an empty class rctangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+
 class Rectangle:
-    """A class representing a rectangle.
-
-    This class defines a rectangle but does not currently
-    include any methods or attributes.
-
-    Attributes:
-        None
+    """ an empty class Rectangle that defines a rectangle
     """
 
     pass
