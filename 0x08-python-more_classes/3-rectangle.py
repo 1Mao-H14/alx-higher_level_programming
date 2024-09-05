9dc799017aca5a74727818846bb35a5556d3dc38
marwan <marwanhamdaoui2020@gmail.com>
2024-09-05 16:35:09 -0700

    task 3 : done

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+""" defines Rectangle class """
+
+class Rectangle:
+    """
+    Class Rectangle that defines a rectangle
+    Attributs:
+
+        width (int): the width of the rectangle
+        height (int): the height of the rectangle
+
+    """
+    def __init__(self, width=0, height=0):
+        """
+        initialising a new Rectangle
+            Args:
+                  width (int): the widht of the Rectangle
+                  height(int) : the height of the Rectangle
+        """
+        self.width = width
+        self.height = height
+    """ widht and height getter """
+    @property
+    def width(self):
+        """
+         the widht of the object
+        Returns:
+            int: the widht of the Rectangle object
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        the height of the rectangle
+        Returns:
+            int : the height of the Rectangle object
+        """
+        return self.__height
+
+    """ widht and height setter """
+    @width.setter
+    def width(self, value):
+        """
+        setting the widht value to an Rectangle object
+        Args:
+            value (int): the widht of the rectangle
+        Raises:
+            ValueError : if the value is < 0
+            TypeError: if value is not an integer
+        """
+        if (type(value) is int):
+            if (value >= 0):
+                self.__width = value
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+    @height.setter
+    def height(self, value):
+        """
+        setting the height value to an Rectangle object
+        Args:
+            value (int): the height of the rectangle
+        Raises:
+            ValueError : if the value is < 0
+            TypeError: if value is not an integer
+        """
+        if (isinstance(value, int)):
+            if (value >= 0):
+                self.__height = value
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
+
+    def area(self):
+        """
+        Calculate the area of a Rectangle
+
+        Returns:
+             int : the area of a Rectangle
+        """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """
+        Calculate the perimeter of a Rectangle
+        Returns:
+             int : the perimeter of a Rectangle
+        """
+        if (self.__height == 0 or self.__width ) == 0:
+            return 0
+        else:
+            return (2*(self.__height + self.__width))
+
+    def __str__(self):
+        """
+        printing the string
+
+        Return:
+            str : the string #
+        """
+        c = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                c += '#'
+            c += '\n'
+        return c.strip()
