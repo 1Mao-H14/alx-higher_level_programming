69f39c3ed725e4cb92e1d322f6f1008da14aa94e
marwan <marwanhamdaoui2020@gmail.com>
2024-09-07 11:28:00 -0700

    task 7 : Done

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -15,7 +15,8 @@ class Rectangle:
 
     """
     number_of_instances = 0
-    print_symbol = '#'
+    print_syimbol = '#'
+
     def __init__(self, width=0, height=0):
         """
         initialising a new Rectangle
