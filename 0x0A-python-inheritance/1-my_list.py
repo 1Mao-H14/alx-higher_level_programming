e8856f1c998997c14416636c0beb4c43c1dc38a1
marwan <marwanhamdaoui2020@gmail.com>
2024-11-07 11:44:49 -0800

    task 1:test

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" A module thats containes the sorting function
+Classes:
+    classe thats represent a a list
+Function:
+    print_sorted(self): return's sorted list
+"""
+
+
+class MyList(list):
+    """
+    classe thats represnt a list
+
+        Attributes:(no attributes)
+        Methods:
+            print_sorted(self):return's the sorted list
+            """
+    def print_sorted(self):
+        self.sort()
