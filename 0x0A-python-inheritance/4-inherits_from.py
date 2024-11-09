63066c1b6eccb4df5d16f05361160a4464e0f6e6
marwan <marwanhamdaoui2020@gmail.com>
2024-11-09 13:18:11 -0800

    testin 1 : task 5

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+A Module thats containes is_kind_of_class(obj, a_class) function
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    a function thats  returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    Args:
+        obj(object): argument object
+        a_class(class): argument class
+    Return:
+        True : if the object is an instance of,
+        or if the object is an instance of a class that inherited from,
+        the specified class
+        False : if not
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
