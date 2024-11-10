429acc6c147d9f1f5205da567854bb1b100c62b4
marwan <marwanhamdaoui2020@gmail.com>
2024-11-10 15:35:12 -0800

    task 7 : done

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 BaseGeometry = __import__(7-base_geometry.py).BaseGeometry
 
-""" A Module thats containes Rctangle class
+"""A Module thats containes Rctangle class
 """
 
 
 class Rectangle(BaseGeometry):
 
-    """A class thats represent A Rectangle
-    """
+    """Rectangle class"""
 
     def __init__(self, width, height):
         super().integer_validator("width", width)
