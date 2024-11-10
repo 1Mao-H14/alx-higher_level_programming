793530a9b526b33e0dcac7c9ac338a834503fbfe
marwan <marwanhamdaoui2020@gmail.com>
2024-11-10 07:43:45 -0800

    task 4 : done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -19,7 +19,7 @@ def inherits_from(obj, a_class):
         the specified class
         False : if not
     """
-    if type(obj).__name__ != a_class:
+    if type(obj) != a_class:
         return True
     else:
         return False
