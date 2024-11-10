4d40829353adae4c6e4b5d65974290cff64bc53f
marwan <marwanhamdaoui2020@gmail.com>
2024-11-10 08:53:39 -0800

    task 5 : done

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,26 +3,28 @@
 """
 4-inherits_from.py
 
-This script defines a function `inherits_from` that checks if an object is an 
-instance of a class that inherits from a specified class (but not the class itself).
+This script defines a function `inherits_from` that checks
+if an object is an
+instance of a class that
+inherits from a specified class (but not the class itself).
 
 Function:
-
-- inherits_from(obj, a_class):
-    Returns `True` if `obj` is an instance of a class that inherits from `a_class`,
-    and `False` if it is either not an instance or is exactly an instance of `a_class`.
+    inherits_from(obj, a_class)
 
 Args:
-    obj (object): The object to check.
-    a_class (class): The class to check against.
+    obj (object): The object
+    a_class (class): The class
 
 Returns:
-    bool: True if `obj` is an instance of a subclass of `a_class` but not `a_class` itself.
+    bool: True if `obj` is an instance of a subclass of `a_class`
+but not `a_class` itself.
 """
 
 def inherits_from(obj, a_class):
     """
-    Returns True if `obj` is an instance of a subclass of `a_class`, but not `a_class` itself.
+    Returns True if `obj` is an 
+    instance of a subclass of `a_class`
+    ,but not `a_class` itself.
     """
     if isinstance(obj, a_class):
         return type(obj) != a_class and issubclass(type(obj), a_class)
