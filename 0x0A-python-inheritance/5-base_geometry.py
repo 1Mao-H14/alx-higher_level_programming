65adde06e2d1af0dadf1e02538559487ef4ae221
marwan <marwanhamdaoui2020@gmail.com>
2024-11-10 07:55:44 -0800

    task 4 : done

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+A Module thats containes is_kind_of_class(obj, a_class) function
+"""
+
+class BaseGeometry():
+    """
+    a function thats  returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False.
+    Args:
+        obj(object): argument object
+        a_class(class): argument class
+    Return:
+        True : if the object is an instance of,
+        or if the object is an instance of a class that inherited from,
+        the specified class
+        False : if not
+    """
+    pass
