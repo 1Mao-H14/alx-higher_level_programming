1c5549480f9b1fafaa1383837c92df7bf8b1b1c0
marwan <marwanhamdaoui2020@gmail.com>
2024-11-10 14:26:02 -0800

    task 7 : done

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -24,7 +24,7 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         elif (value <= 0):
-            raise ValueError('{} must be greater than 0'.format(value))
+            raise ValueError('{} must be greater than 0'.format(name))
 
     def area(self):
         """ raises an Exception """
