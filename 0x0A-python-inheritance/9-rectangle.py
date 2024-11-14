e48fc1729c8d9ffc7ca34ed7a87ddf602300e7fa
marwan <marwanhamdaoui2020@gmail.com>
2024-11-14 14:25:06 -0800

    task 9 : done

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -5,7 +5,10 @@ BaseGeometry =__import__.(8-rectangle.py).BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
-    super().__init__(self, width, height)
-    super().area()
+    def __init__(self, width, height):
+        super().__init__(self, width, height)
+    def area(self):
+    """Override area() to calculate the rectangle's area"""
+        return self.width * self.height
     def __str__(self):
         return '[Rectangle] {width}/{height}'.format(self.width ,self.height)
