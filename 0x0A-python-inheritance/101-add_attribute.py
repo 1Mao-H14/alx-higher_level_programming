5a7ee48816f1ee842f9216e722ec1cf3f86a9784
marwan <marwanhamdaoui2020@gmail.com>
2024-11-16 09:44:11 -0800

    task 12 :done

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+"""
+Module to dynamically add an attribute to an object.
+
+This module defines a function `add_attribute` that attempts to add a new
+attribute to an object if the object is mutable. If the object is of an
+immutable type (such as int, str, tuple, or float) or a subclass of these
+types, a TypeError is raised.
+
+The `add_attribute` function can be used to modify objects at runtime by
+adding new attributes to them.
+
+Usage Example:
+    class MyClass:
+        pass
+    my_obj = MyClass()
+    add_attribute(my_obj, "name", "John")
+    print(my_obj.name)  # Outputs: John
+"""
+
+
 def add_attribute(obj, att, val):
     """
     Adds a new attribute to an object if allowed.
