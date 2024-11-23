ffc3667ecd80856e04bf96ba438f6ae913dd784a
marwan <marwanhamdaoui2020@gmail.com>
2024-11-23 15:03:14 -0800

    task 2 : test

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A MODULE Thats containes append_write function
+"""
+
+
+def append_write(filename="", text=""):
+    """"
+    append_write : that appends a string at the end of a text file
+
+    Args:
+        filename(str) : the file name
+        text(str) : the text entred
+
+    Return:
+        int : the number of characters added
+    """
+    with open(filename, mode='w', encoding='utf-8') as f:
+        wr_t = f.write(text)
+        f.seek(0)
+        ln_txt = len(f.read())
+        return ln_txt
