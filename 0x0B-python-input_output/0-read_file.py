c085a62ad0de0322f955c1efb6d3fb78ac1dffce
marwan <marwanhamdaoui2020@gmail.com>
2024-11-23 12:43:17 -0800

    task 0 : test

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+A MODULE Thats containes read_file function
+"""
+def read_file(filename=""):
+    """
+    A Function that reads a text file (UTF8) and prints it to stdout:
+    filename(str): the file name
+
+    Return:
+        None : nothing
+    """
+    with open(filename, mode = 'r', encoding = 'utf-8') as f:
+        f_read = f.read()
+        print(f_read)
