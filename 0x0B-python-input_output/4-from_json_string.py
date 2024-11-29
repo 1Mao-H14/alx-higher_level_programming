cdec06df3089a748f31bece3f42ba00131eac801
marwan <marwanhamdaoui2020@gmail.com>
2024-11-29 14:33:46 -0800

    task 4: testing the output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A MODULE Thats containes from_json_string function
+"""
+
+
+import json
+
+def from_json_string(my_str):
+
+
+    """
+    a function that returns an object (Python data structure)
+    represented by a JSON string
+    Args:
+        my_str (str): the string argument to be serialised
+    Returns:
+        object : an object
+    """
+    l_dile = json.load(my_str)
+    return l_dile
