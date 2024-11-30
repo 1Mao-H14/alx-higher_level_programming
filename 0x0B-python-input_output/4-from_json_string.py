ee95ddfe43d4a637417adcea39566e38eb0697cc
marwan <marwanhamdaoui2020@gmail.com>
2024-11-29 16:44:04 -0800

    task 4: testing the output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
+
+
 """
 A MODULE Thats containes from_json_string function
 """
 
-
 import json
 
+
 def from_json_string(my_str):
-
-
     """
     a function that returns an object (Python data structure)
     represented by a JSON string
