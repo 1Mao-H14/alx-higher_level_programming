4514980e42863c8149ec52c1b50f10905c644376
marwan <marwanhamdaoui2020@gmail.com>
2024-12-01 13:56:15 -0800

    task 9 : test

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" A module thats containes student class
+"""
+
+
+class Student:
+    """
+    a class that represent student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+
+        """
+        Public method that retrieves a
+        dictionary representation of a Student
+        """
+        return self.__dict__
