d82e8d27c5f8d5c243eeab43a2a2b89f3a4b5666
marwan <marwanhamdaoui2020@gmail.com>
2024-12-08 14:31:55 -0800

    final task :test

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""""A module which containes student class and to_json function"""
+
+
+from sys import stdini
+from collections import Counter
+
+
+file = stdin
+lines = file.readlines()
+list_val = []
+size = 0
+c = 0
+try:
+    for i in lines:
+        if c < 10:
+            ln_val = i.strip().split()
+            size += int(ln_val[-1])
+            list_val.append(int(ln_val[-2]))
+            c += 1
+    c1 = Counter(list_val)
+    # sorting the dict
+    map_val = dict(c1)
+    sorted_key_map = sorted(map_val)
+    new_sorted = {}
+    for e in sorted_key_map:
+        new_sorted[e] = map_val[e]
+
+    c = 0
+    print('File size: {:d}'.format(size))
+    for s, c in new_sorted.items():
+        print('{:d}: {:d}'.format(s, c))
+
+except KeyboardInterrupt:
+    c = 0
+    print('File size: {:d}'.format(size))
