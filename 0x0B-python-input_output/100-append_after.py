eaefc724a336d9018b7b8dcbf66ef575aaa23f43
marwan <marwanhamdaoui2020@gmail.com>
2024-12-08 04:58:32 -0800

    task 100: test

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""""A module which containes student class and append_after function"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Appends a new string after lines containing a specific
+    search string in a file.
+
+    Args:
+        filename (str): Name of the file to modify.
+        search_string (str): String to search for in the file's lines.
+        new_string (str): String to append after matching lines.
+    Return:
+        none : nothing
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    with open(filename, 'w', encoding='utf-8') as w:
+        for line in lines:
+            w.write(line+'\n')
+            if search_string in line:
+                w.write(new_string+'\n')
+            else:
+                pass
