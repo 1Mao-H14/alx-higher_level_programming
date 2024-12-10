6d4b25b70e4df9abb880f31d0990aa968241653f
marwan <marwanhamdaoui2020@gmail.com>
2024-12-10 13:51:09 -0800

    test out2 : try

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,55 +1,62 @@
 #!/usr/bin/python3
 
-"""A Module which containes Rectangle class"""
+"""Defines the Rectangle class, inheriting from Base."""
 
 from base import Base
 
 
 class Rectangle(Base):
-    """"A class thats represent a Rectangle"""
+    """Represents a rectangle."""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a Rectangle.
+
+        Args:
+            width (int): Rectangle width.
+            height (int): Rectangle height.
+            x (int): x-coordinate (default 0).
+            y (int): y-coordinate (default 0).
+            id (int, optional): Object ID.
+        """
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
-        super.__init__(id)
+        super().__init__(id)
 
-    # width getter property
     @property
     def width(self):
-        self.__width
+        """int: Rectangle width."""
+        return self.__width
 
-    # width setter
     @width.setter
     def width(self, value):
         self.__width = value
 
-    # height getter
     @property
     def height(self):
+        """int: Rectangle height."""
         return self.__height
 
-    # height setter
     @height.setter
     def height(self, value):
         self.__height = value
 
-    # x getter
     @property
     def x(self):
+        """int: x-coordinate."""
         return self.__x
 
-    # x setter
     @x.setter
     def x(self, value):
         self.__x = value
 
-    # y getter
     @property
     def y(self):
+        """int: y-coordinate."""
         return self.__y
 
-    # y setter
     @y.setter
     def y(self, value):
         self.__y = value
