832d4d671a76d8c7a42bd69351ab5b0785b9d86a
marwan <marwanhamdaoui2020@gmail.com>
2024-12-11 10:01:51 -0800

    task <1. Base class> ==>  done : testing out

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""""a module which containes tests of the base class"""
+import module.base
+import unittest
+
+
+class TestBase(unittest.TestCase):
+    def test_class_creation(self):
+        # simple tests
+        a = Base(None)
+        self.assertEqual(a.id, 1)
+        b = Base(None)
+        self.assertEqual(b.id, 2)
+        c = Base(89)
+        self.assertEqual(c.id, 89)
