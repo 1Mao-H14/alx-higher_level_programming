932e5032d842925899734129273d3e501c514f8b
marwan <marwanhamdaoui2020@gmail.com>
2024-12-11 11:35:48 -0800

    task <1. Base class--test1> ==>  done : testing out

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
 """"a module which containes tests of the base class"""
-from models.base import Base
+import models.base
 import unittest
 
 
 class TestBase(unittest.TestCase):
+    def setUp(self):
+        """Reset __nb_objects before each test."""
+        Base._Base__nb_objects = 0
+
     def test_class_creation(self):
         # simple tests
         a = Base(None)
         self.assertEqual(a.id, 1)
+
+    def test_auto_increment(self):
         b = Base(None)
-        self.assertEqual(b.id, 2)
-        c = Base(89)
-        self.assertEqual(c.id, 89)
-if __name__ == '__main__':
-    unittest.main()
+        c = Base(None)
+        self.assertEqual(b.id + 1, c.id)
+
+    def test_saving_id(self):
+        d = Base(89)
+        self.assertEqual(d.id, 89)
