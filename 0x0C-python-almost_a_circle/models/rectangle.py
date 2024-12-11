7b35eff7da699fa39d5d172b27f452a286be51f3
marwan <marwanhamdaoui2020@gmail.com>
2024-12-11 14:45:00 -0800

    task : 8 (done)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,8 +106,10 @@ class Rectangle(Base):
 
     def display(self):
         """ prints in stdout the Rectangle instance with the character #"""
-        for o in range(self.height):
-            print(self.width * '#')
+        for _ in range(self.y):
+            print()
+        for t in range(self.hight):
+            print(self.x * ' ' + self.width * '#')
 
     def __str__(self):
         msg = '[Rectangle] ({}) {}/{} - {}/{}'
