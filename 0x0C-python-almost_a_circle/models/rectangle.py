cc6e390dda2ce049352d568397e8ef9eb7e4324a
marwan <marwanhamdaoui2020@gmail.com>
2024-12-11 15:16:11 -0800

    task : 8 (done)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,17 @@ class Rectangle(Base):
     def __str__(self):
         msg = '[Rectangle] ({}) {}/{} - {}/{}'
         return msg.format(self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args):
+        """ that assigns an argument to each attribute """
+        for (i, v) in enumerate(args):
+            if i == 0:
+                self.id = v
+            elif i == 1:
+                self.width = v
+            elif i == 2:
+                self.height = v
+            elif i == 3:
+                self.x = v
+            elif i == 4:
+                self.y = v
