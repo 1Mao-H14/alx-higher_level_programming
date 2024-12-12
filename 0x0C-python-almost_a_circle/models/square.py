ea6971a5ceba3fcc334c2c88c34cea95d5223de1
marwan <marwanhamdaoui2020@gmail.com>
2024-12-12 15:14:51 -0800

    lts go ; done

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A module which containes a square class"""
+""" A module which containes a square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """A class representing a square"""
-    def __init__(size, x=0, y=0, id=None):
+    def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
