4e0344b7e2ad6a8465f134fc5145e489f86cdc0b
marwan <marwanhamdaoui2020@gmail.com>
2025-01-12 06:57:01 -0800

    0. cURL body size:test out

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -i -X GET -o file.txt 'http://0.0.0.0:5000'
-# using cut
-grep 'Content-Length' file.txt | cut -d ':' -f 2
+# a Bash script that takes in a URL, sends a request to that URL,and displays the size of the body of the response
+curl -X GET 'http://0.0.0.0:5000' | grep 'Content-Length' file.txt | cut -d ': ' -f 2
